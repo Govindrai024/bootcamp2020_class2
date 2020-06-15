c9f248394ca45f4c934cd539104b4bb97fa7af1d
Govindrai024 <govindrai024@gmail.com>
2020-06-15 23:28:15 +0500

    create react app with build

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  
+     <h1>Hello, I am Govind Rai.</h1>
+    <h2>I have learnt to create react app.</h2>
+    <h3>I have also learn about build command, and finally have deployed my first react code on surge. </h3>
+    
+    
+  
+    </div>
+  );
+}
+
+export default App;
